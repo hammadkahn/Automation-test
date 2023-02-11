688f0140e3b2c140b500054b6973c3b9fe66df04
Hammad Khan <hammad.khan0312@gmail.com>
2023-02-11 17:34:54 +0500

    Test Script python
    Testing script

---
+++ Testcase.py
@@ -0,0 +1,21 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://www.saucedemo.com/")
+assert "Python" in driver.title
+driver.maximize_window()
+time.sleep(2)
+element = driver.find_element(By.ID, "user-name")
+element.send_keys("standard_user")
+time.sleep(2)
+elementp = driver.find_element(By.ID, "password")
+elementp.send_keys("secret_sauce")
+time.sleep(2)
+login_btn = driver.find_element(By.ID, "login-button")
+login_btn.click()
+time.sleep(5)
+
+driver.quit()
